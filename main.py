cb975fa55db9c1fe46ba21b7055ab253090dbb5c
Manuel Manelli <manuelmanelli@icloud.com>
2022-05-18 18:30:16 +0200

    Riorganizzazione file nella repo

---
+++ main.py
@@ -0,0 +1,45 @@
+import descriptive
+import etl
+
+df = etl.importa()
+
+#descriptive.count_one_mode(df, "type_1", "Tipo", "Conteggio", "Conteggi per tipo", "/Users/Manuel/Desktop/grafico1.png")
+#descriptive.count_one_mode(df, "generation", "Generazione", "Conteggio", "Conteggio per generazione", "/Users/Manuel/Desktop/grafico2.png")
+#descriptive.count_one_mode(df, "status", "Status", "Conteggio", "status", "/Users/Manuel/Desktop/grafico3.png")
+
+s = df["status"].value_counts()
+dict = s.to_dict()
+count = list(dict.values())
+pie_sum = 0
+for elem in count:
+    pie_sum = pie_sum + elem
+pie_percentage = []
+for elem in count:
+    pie_percentage.append(round((elem/pie_sum),2))
+max = max(pie_percentage)
+pie = count
+del pie[0]
+bar_sum = 0
+for elem in pie:
+    bar_sum = bar_sum + elem
+bar_percentage = []
+for elem in pie:
+    bar_percentage.append(round((elem/bar_sum),2))
+
+min = 0
+for elem in pie_percentage[1:]:
+    min = min + elem
+
+
+param_pie = {
+    "ratios" : [min, max],
+    "labels" : ["Legendary", "Normal"],
+    "explode" : [0.1, 0]
+}
+param_bar = {
+    "title" : "Legendary",
+    "legend" : list(dict.keys())[1:],
+    "ratios" : bar_percentage
+}
+
+descriptive.bar_of_pie(param_pie, param_bar)
