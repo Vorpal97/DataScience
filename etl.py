48d18ead975a5471027a24815ba23dcb368a6d29
Fiorenza <fiorenzab-97@libero.it>
2022-07-24 19:37:02 +0200

    Modifica ETL Classification

--- etl.py
+++ etl.py
@@ -9,6 +9,22 @@ def importa(dataset):
     return(df)
 
 
+def importa_clf(dataset):
+    df = pd.read_csv(dataset)
+    df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
+    to_drop_list =['pokedex_number', 'name', 'german_name', 'japanese_name', 'generation',
+                   'status', 'species', 'type_number', 'type_2', 'height_m', 'weight_kg',
+                   'abilities_number', 'ability_1', 'ability_2', 'ability_hidden', 'catch_rate', 'base_friendship', 'base_experience',
+                   'growth_rate', 'egg_type_number', 'egg_type_1', 'egg_type_2', 'percentage_male', 'egg_cycles',
+                   'against_normal', 'against_fire', 'against_water', 'against_electric',
+                   'against_grass', 'against_ice', 'against_fight', 'against_poison',
+                   'against_ground', 'against_flying', 'against_psychic', 'against_bug',
+                   'against_rock', 'against_ghost', 'against_dragon', 'against_dark',
+                   'against_steel', 'against_fairy']
+    df = df.drop(columns = to_drop_list)
+    return(df)
+
+
 def create_crypto_dict(df, coins):
     df_list = {}
     for elem in coins:
@@ -33,6 +49,5 @@ def add_avg_column(df, coins_to_keep):
     for elem in coins_to_keep:
         df[elem]['Avg'] = (df[elem]['High'] + df[elem]['Low'])/2
     return df
-
 
 
